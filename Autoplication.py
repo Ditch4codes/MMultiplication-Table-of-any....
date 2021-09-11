ce4d6b89027d69832a1ca7ab0544150497d3c498
Ditch4codes <90508184+Ditch4codes@users.noreply.github.com>
2021-09-11 18:30:42 +0300

    Add files via upload

---
+++ Autoplication.py
@@ -0,0 +1,9 @@
+print("<>"*20)
+print("Life made simpler for primary grade students")
+print("multi-autoplication")
+print("<>"*20)
+num=int(input("Enter the num:"))
+for a in range(1,11):
+    mult=num*a
+    print(num,"x",a,"=",mult)
+print("#Welcome back in advance#")    
